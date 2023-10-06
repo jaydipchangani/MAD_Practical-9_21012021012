b92fa4958e8cc95727185b73c6761593222ffe03
student <student@gnu.ac.in>
2023-10-06 08:47:50 +0530

    Commit

--- app/src/main/java/com/example/mad_practical_9_21012021012/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021012/MainActivity.kt
@@ -1,32 +1,38 @@
-package com.example.mad_practical_9_21012021012
+package com.example.mad_practical_9_21012021012.R
 
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.mad_practical_9_21012021012.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation : AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var alarmanimation: AnimationDrawable
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val alarm: ImageView =findViewById(R.id.imagealarm)
+        val alarm : ImageView = findViewById(R.id.alarm)
         alarm.setBackgroundResource(R.drawable.alarm_animation_list)
-        alarmanimation=alarm.background as AnimationDrawable
+        alarmanimation = alarm.background as AnimationDrawable
 
-
+        val heart : ImageView = findViewById(R.id.heart)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation = heart.background as AnimationDrawable
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
-        if(hasFocus){
-            alarmanimation.start()
-        }
-        else{
-            alarmanimatin.stop()
-        }
         super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+            heartanimation.start()
+        }
+        else {
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
     }
 }
--- app/src/main/java/com/example/mad_practical_9_21012021012/SplashActivity.kt
+++ app/src/main/java/com/example/mad_practical_9_21012021012/SplashActivity.kt
@@ -1,11 +1,36 @@
-package com.example.mad_practical_9_21012021012
+package com.example.mad_practical_9_21012012012
 
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.mad_practical_9_21012021012.R
+import android.widget.ImageView
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(),Animation.AnimationListener {
+    lateinit var logoimage:ImageView
+    lateinit var logoanimation : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        logoimage=findViewById(R.id.uvpce)
+        logoimage.setBackgroundResource((R.drawable.uvpce_logo_list))
+        logoanimation=AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        logoanimation.setAnimationListener(this)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        //intent use to open animation
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
     }
 }
